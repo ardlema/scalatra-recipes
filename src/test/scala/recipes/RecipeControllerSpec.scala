7233f64b493e47d581fe2a07d2b58da8e09cc209
Alberto Rodriguez <ardlema@gmail.com>
2012-11-17 23:23:36 +0000

    Test for the recipe API

---
+++ src/test/scala/recipes/RecipeControllerSpec.scala
@@ -0,0 +1,34 @@
+package recipes
+
+import org.scalatra.test.specs2.ScalatraSpec
+
+
+/**
+ * Set of Unit test to test our API
+ */
+class RecipeControllerSpec extends ScalatraSpec {
+
+  // add the filter so we can start testing
+  addFilter(classOf[RecipeController], "/*")
+
+  // some constants
+  val EXPECTED_RECIPES = """{"name":"Pollo en escabeche","difficulty":"Facil","elaborationTime":0.5,"ingredients":[{"name":"pollo"},{"name":"cebolla"},{"name":"zanahoria"},{"name":"aceite"}],"price":20,"links":[{"linkType":"application/vnd.smartbid.item","rel":"Add recipe","href":"/users/123/addRecipe"}]}"""
+  val RECIPE_URL = "/recipes/123";
+  val MED_TYPE = "application/json"
+
+
+  def is =
+    "Calling an unknown url on the API "               ^
+      "returns status 404"                            ! statusResult("/recipes/567",404)^
+      end ^ p ^
+    "Calling a GET on " + RECIPE_URL + " should"       ^
+      "return status 200"                             ! statusResult(RECIPE_URL,200)^
+      "and body should equal: " + EXPECTED_RECIPES        ! {get(RECIPE_URL){response.body must_== EXPECTED_RECIPES}}^
+      "and media-type should equal: " + MED_TYPE		! {get(RECIPE_URL){response.getContentType must startWith(MED_TYPE)}}
+  end
+
+  def statusResult(url:String,code:Int) =
+    get(url) {
+      status must_== code
+    }
+}
